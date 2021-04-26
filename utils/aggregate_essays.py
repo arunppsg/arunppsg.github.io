06fbfe643addfaf335f55087e4a24940331a54a2
arunpalaniappan <arun.palaniappan1999@gmail.com>
2021-04-26 11:24:50 +0530

    Added utility script for adding entries into index

--- utils/aggregate_essays.py
+++ utils/aggregate_essays.py
@@ -7,8 +7,68 @@ It gets title from the blog name, date from os.getmtime utility
 
 import os
 import datetime
-with open("../index.html", "r") as f:
-    lines = f.readlines()
+
+header = """
+<html>
+
+<head>
+    <!-- Tamil Quote -->
+
+    <script>
+    // https://stackoverflow.com/questions/49468425/js-pick-random-quotes-on-page-refresh
+    function PickThirukkural()
+    {
+        var kurals[10];
+        alert("Alert");
+    }
+    </script>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-M6KVQ3TNZY"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-M6KVQ3TNZY');
+    </script>
+    <title>Home</title>
+    <link rel = "stylesheet" type = "text/css" href = "index.css" />
+</head>
+"""
+
+active_work = """
+<body>
+    <div class="thirukkual">
+    </div>
+
+    <div class="container">
+        <div class="content">
+            <div class="active-works">
+                <h2>Active Works</h2>
+                <ul class="active-works-list">
+                    <li>
+                        <br><span class="active-works-link">Working on detecting malware from network traffic.</span>
+                    </li>
+                </ul>
+            </div>
+"""
+
+papers = """
+            <div class="papers">
+                <h2>Unpublished Manuscripts</h2>
+                <ul class="paper-list">
+                <li>
+                    <a class="paper-link" href="files/FCI.pdf">Data-Driven Analysis of Food Corporation of India’s Operations and Recommendations</a>(Submitted to DSE Winter School 2020)
+                </li>
+                </ul>
+            </div>
+"""
+
+essay_list_start = """
+            <div class="essays">
+                <h2>Essays</h2>
+                <ul class="essay-list">
+"""
 
 def get_starting_lines():
     start_lines = []
@@ -30,7 +90,7 @@ def get_essay_link_line(file_path):
     file_name = file_path.split('/')[2]
     dir_name = file_path.split('/')[1]
     line += dir_name + '/' + file_name
-    line += '">'
+    line += '>'
     heading = file_name.replace('-', ' ').replace('.html', '')
     heading = heading.split(' ')
     for i, word in enumerate(heading):
@@ -42,23 +102,57 @@ def get_essay_link_line(file_path):
     line += '</a>\n'
 
     return line
-                
+   
+
 dirs = ['2020', '2021']
+file_list = []
+for dir in dirs:
+    for file in os.listdir(os.path.join('..', dir)):
+        file_path = os.path.join(os.path.join('..', dir), file)
+        file_list.append([file_path, os.path.getmtime(file_path)])
+file_list.sort(key = lambda x: x[1], reverse=True)
+
 new_lines = []
-for line in lines:
-    new_lines.append(line)
-    if '<ul class="essay-list">' in line:
-        for dir in dirs:
-            for file in os.listdir(os.path.join('..', dir)):
-                file_path = os.path.join(os.path.join('..', dir), file)
-                new_lines.extend(get_starting_lines())
-                new_lines.extend(get_date_line(file_path))
-                new_lines.extend(get_essay_link_line(file_path))
-                new_lines.extend('<span class="tags"> </span>\n')
-                new_lines.extend('</div>\n</li>\n')
+for file in file_list:
+    new_lines.extend(get_starting_lines())
+    new_lines.extend(get_date_line(file[0]))
+    new_lines.extend(get_essay_link_line(file[0]))
+    new_lines.extend('<span class="tags"> </span>\n')
+    new_lines.extend('</div>\n</li>\n')
+
+footer = """
+       </ul>
+       </div>
+        </div>
+       <div class="sidebar">
+            <span class='about'>
+                <a href="about.html">About me</a>
+                <br><br>
+                <a href="bookshelf.html">Bookshelf</a>
+            </span>
+        </div>
+    </div>
+    <a href="https://info.flagcounter.com/SG6L"><img src="https://s05.flagcounter.com/count2/SG6L/bg_FFFFFF/txt_000000/border_CCCCCC/columns_5/maxflags_25/viewers_0/labels_0/pageviews_0/flags_0/percent_0/" alt="Flag Counter" border="0"></a>
+    <br>
+    <div class="footer">
+        Arun Palaniappan © 2021. Last update: 28 March 2021.
+    </div>
+
+</html>
+
+</body>
+
+</html>
+
+"""
 
 with open('../index.html', 'w') as f:
+    f.write(header)
+    f.write(active_work)
+    f.write(papers)
+    f.write(essay_list_start)
     f.writelines(new_lines)
+    f.write(footer)
 
 
 
